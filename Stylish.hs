b26bb3d190dcd3f81b5acc6c288afc3645dc1420
Adrian Cochrane <adrian@openwork.nz>
2019-06-11 19:29:49 +1200

    Implement basic parser. TODO test.

---
+++ Stylish.hs
@@ -0,0 +1,8 @@
+module Stylish(
+        parse,
+        StyleSheet, addRule, addAtRule,
+        StyleRule,
+        Selector, Combinator, SimpleSelector, AttrTest
+    ) where
+
+import Stylish.Parse
